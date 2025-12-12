d6ec3f4c341203c2bfdc2384022673f24abce677
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-12-12 01:37:03 +0000

    feat(application): support attachments in create question use case
    Updates the  to allow associating attachments when creating a new question.
    
    Key changes:
    - Adds  to the input request DTO.
    - Maps the provided IDs to  entities.
    - Initializes a  and assigns it to the  aggregate before persisting it to the repository.

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -2,11 +2,14 @@ import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Question } from "../../enterprise/entities/question";
 import { QuestionsRepository } from "../repositories/questions-repository";
 import { Either, right } from "@/core/either";
+import { QuestionAttachment } from "../../enterprise/entities/question-attachment";
+import { QuestionAttachmentList } from "../../enterprise/entities/question-attachment-list";
 
 interface CreateQuestionUseCaseRequest {
   title: string;
   content: string;
   authorId: string;
+  attachmentsIds: string[];
 }
 
 type CreateQuestionUseCaseResponse = Either<null, { question: Question }>;
@@ -18,12 +21,22 @@ export class CreateQuestionUseCase {
     title,
     content,
     authorId,
+    attachmentsIds,
   }: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
     const question = Question.create({
       title,
       content,
       authorId: new UniqueEntityID(authorId),
     });
+
+    const questionAttachments = attachmentsIds.map((attachmentId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        questionId: question.id,
+      });
+    });
+
+    question.attachments = new QuestionAttachmentList(questionAttachments);
 
     await this.questionsRepository.create(question);
 
