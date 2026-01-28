46e0e756d946386f944c0f19cb8a7dcb406a104e
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2026-01-28 22:37:13 +0000

    feat(application): support attachments in answer question use case
    Updates the  to allow associating attachments when creating a new answer.
    
    Key changes:
    - Adds  to the request DTO.
    - Maps provided IDs to  entities and assigns them to the Answer aggregate.
    - Updates unit tests to inject the attachments repository and verify that attachments are correctly persisted.

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,13 +1,20 @@
 import { beforeEach, describe, expect, it, test } from "vitest";
 import { AnswerQuestionUseCase } from "./answer-question";
 import { InMemoryAnswersRepository } from "@/test/repositories/in-memory-answers-repository";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { InMemoryAnswerAttachmentsRepository } from "@/test/repositories/in-memory-answer-attachments-repository";
 
 let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository;
 let sut: AnswerQuestionUseCase;
 
 describe("Create an answer", () => {
   beforeEach(() => {
-    inMemoryAnswersRepository = new InMemoryAnswersRepository();
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository();
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(
+      inMemoryAnswerAttachmentsRepository
+    );
     sut = new AnswerQuestionUseCase(inMemoryAnswersRepository);
   });
 
@@ -16,9 +23,21 @@ describe("Create an answer", () => {
       instructorId: "1",
       questionId: "1",
       content: "Conteúdo da resposta",
+      attachmentsIds: ["1", "2", "4"],
     });
 
     expect(result.isRight()).toBe(true);
     expect(inMemoryAnswersRepository.items[0]).toEqual(result.value?.answer);
+    expect(
+      inMemoryAnswersRepository.items[0].attachments.currentItems
+    ).toHaveLength(3);
+
+    expect(inMemoryAnswersRepository.items[0].attachments.currentItems).toEqual(
+      [
+        expect.objectContaining({ attachmentId: new UniqueEntityID("1") }),
+        expect.objectContaining({ attachmentId: new UniqueEntityID("2") }),
+        expect.objectContaining({ attachmentId: new UniqueEntityID("4") }),
+      ]
+    );
   });
 });
