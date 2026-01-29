f77d91b3dc6e18dd3755902482366117dae5e6f6
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2026-01-29 01:05:48 +0000

    test(application): update dependencies in comment on answer test
    Injects the  into the answers repository setup () to align with the updated constructor signature.

--- src/domain/forum/application/use-cases/comment-on-answer.spec.ts
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -3,14 +3,20 @@ import { CommentOnAnswerUseCase } from "./comment-on-answer";
 import { InMemoryAnswersRepository } from "@/test/repositories/in-memory-answers-repository";
 import { makeAnswer } from "@/test/factory/make-answer";
 import { InMemoryAnswerCommentsRepository } from "@/test/repositories/in-memory-answer-comments-repository";
+import { InMemoryAnswerAttachmentsRepository } from "@/test/repositories/in-memory-answer-attachments-repository";
 
 let inMemoryAnswersRepository: InMemoryAnswersRepository;
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository;
 let sut: CommentOnAnswerUseCase;
 
 describe("Comment on answer", () => {
   beforeEach(() => {
-    inMemoryAnswersRepository = new InMemoryAnswersRepository();
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository();
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(
+      inMemoryAnswerAttachmentsRepository
+    );
     inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository();
     sut = new CommentOnAnswerUseCase(
       inMemoryAnswersRepository,
