46331f8ec51e90ddbfbe05eaa56776bc054ba180
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2026-01-29 01:17:27 +0000

    feat(core): implement equality check in entity base class
    Adds the  method to the abstract  class.
    
    This enables domain logic to verify if two entity instances represent the same underlying identity by comparing their references or their unique IDs.

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -12,4 +12,25 @@ export abstract class Entity<Props> {
     this.props = props;
     this._id = id ?? new UniqueEntityID();
   }
+
+  // Método que verifica se duas entidades representam a mesma coisa no domínio.
+  public equals(entity: Entity<any>){
+
+    // Aqui é comparação por referência, se são instâncias iguais.
+    if(entity === this){
+      return true
+    }
+
+    // Isso verifica se:
+    // - São instâncias diferentes
+    // - Mas possuem o mesmo ID
+    if(entity.id === this._id) {
+      return true
+    }
+
+    // Se:
+    // Não é a mesma instância
+    // Não tem o mesmo ID
+    return false
+  }
 }
