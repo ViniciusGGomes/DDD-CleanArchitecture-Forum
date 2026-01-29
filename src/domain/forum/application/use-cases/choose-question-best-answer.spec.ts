d5172d3872630c971dd741885f7e1fadc6d1a12a
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2026-01-29 01:07:36 +0000

    test(application): update dependencies in choose best answer test
    Injects the attachment repositories into the questions and answers repository setup () to align with the updated constructor signatures.
    
    Also fixes the import path for .

--- src/domain/forum/application/use-cases/choose-question-best-answer.spec.ts
+++ src/domain/forum/application/use-cases/choose-question-best-answer.spec.ts
@@ -5,16 +5,28 @@ import { ChooseQuestionBestAnswerUseCase } from "./choose-question-best-answer";
 import { makeQuestion } from "@/test/factory/make-question";
 import { makeAnswer } from "@/test/factory/make-answer";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import { NotAllowedError } from "./erros/not-allowed-error";
+import { NotAllowedError } from "@/core/erros/erros/not-allowed-error";
+import { InMemoryQuestionAttachmentsRepository } from "@/test/repositories/in-memory-question-attachments-repository";
+import { InMemoryAnswerAttachmentsRepository } from "@/test/repositories/in-memory-answer-attachments-repository";
 
 let inMemoryAnswersRepository: InMemoryAnswersRepository;
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let inMemoryQuestionaAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository;
 let sut: ChooseQuestionBestAnswerUseCase;
 
 describe("Choose question best answer", () => {
   beforeEach(() => {
-    inMemoryAnswersRepository = new InMemoryAnswersRepository();
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository();
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(
+      inMemoryAnswerAttachmentsRepository
+    );
+    inMemoryQuestionaAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository();
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionaAttachmentsRepository
+    );
     sut = new ChooseQuestionBestAnswerUseCase(
       inMemoryAnswersRepository,
       inMemoryQuestionsRepository
